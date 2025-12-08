aff3472ce77021faaf801152e68739ed6c382864
Dmitrii Shikhaleev <padavankun@gmail.com>
2025-12-08 07:45:13 +0100

    add day8

--- day08.lisp
+++ day08.lisp
@@ -7,9 +7,21 @@
  		)
     )
 
+(defun split-by-comma (string)
+	(loop
+	    for i = 0 then (1+ j)
+		as j = (position #\, string :start i)
+		collect (subseq string i j)
+		while j
+    	)
+ 	)
+
 
 (defun main ()
-	(format t "~a~%" "--- Day 8: ---")		
+	(format t "~a~%" "--- Day 8: Playground ---")
+ 	(let ((boxes-list (mapcar #'(lambda (cur) (split-by-comma cur)) (get-file "day08.txt"))))
+ 		(format t "boxes-list: ~a~%" boxes-list)
+	    )
 	)
 
 (main)
